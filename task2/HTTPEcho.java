06dce31bb2b25b15788b7dbcb8ebde6662d49c3c
Agriad <agriad1@yahoo.com>
2019-02-21 19:14:50 +0100

    0.3

--- task2/HTTPEcho.java
+++ task2/HTTPEcho.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class HTTPEcho {
     public static void main( String[] args) throws IOException {
@@ -8,6 +9,8 @@ public class HTTPEcho {
         boolean on = true;
 
         ServerSocket welcomeSocket = new ServerSocket(Integer.valueOf(args[0]));
+        //PrintWriter pwIn = new PrintWriter("in.txt", StandardCharsets.UTF_8);
+        //PrintWriter pwOut = new PrintWriter("out.txt", StandardCharsets.UTF_8);
 
         while (true) {
             on = true;
@@ -16,15 +19,30 @@ public class HTTPEcho {
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             StringBuilder br = new StringBuilder();
 
+            br.append("HTTP/1.1 200 OK\r\n").append("\r\n");
+
             while (on) {
                 clientSentence = inFromClient.readLine();
-                br.append(clientSentence + "\n");
+                br.append(clientSentence + "\r\n");
+                //pwIn.print(clientSentence + "\r\n");
                 if (clientSentence.equals("")) {
                     on = false;
                 }
             }
 
             clientSentence = br.toString();
+
+            /*
+            pwOut.print(clientSentence);
+
+            pwIn.flush();
+            pwOut.flush();
+
+            pwIn.close();
+            pwOut.close();
+            */
+
+            //System.out.println(clientSentence);
             outToClient.writeBytes(clientSentence);
             connectionSocket.close();
         }

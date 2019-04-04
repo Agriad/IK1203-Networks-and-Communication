e9ce040921bbf33d2a2764aa3dc17891dbdf6a89
Agriad <agriad1@yahoo.com>
2019-04-04 10:17:16 +0200

    ???

---
+++ task4/ConcHTTPAsk.java
@@ -0,0 +1,105 @@
+import java.net.*;
+import java.io.*;
+
+import tcpclient.TCPClient;
+
+public class ConcHTTPAsk {
+
+    public static void main(String[] args) throws java.io.IOException {
+        // Your code here
+        int portNum = Integer.valueOf(args[0]);
+
+        ServerSocket welcomeSocket = new ServerSocket(portNum);
+
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+
+            new Thread(() -> {
+                try {
+                    String clientSentence = "";
+                    boolean on = true;
+                    BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                    DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                    StringBuilder br = new StringBuilder();
+
+                    br.append("HTTP/1.1 200 OK\r\n").append("\r\n");
+
+                    while (on) {
+                        clientSentence = inFromClient.readLine();
+                        br.append(clientSentence + "\r\n");
+                        if (clientSentence.equals("")) {
+                            on = false;
+                        }
+                    }
+
+                    clientSentence = br.toString();
+
+                    System.out.println(clientSentence);
+
+                    String[] request = clientSentence.split("\n");
+
+                    String firstLine = request[2];
+
+                    String[] firstLineSplit = firstLine.split(" ");
+                    String check = firstLineSplit[1];
+                    String isItAsk = "";
+                    String serverOutput = null;
+
+                    if (!check.isEmpty() && check.length() >= 4) {
+                        isItAsk = check.substring(1, 4);
+                    }
+
+                    if (isItAsk.equals("ask")) {
+                        try {
+                            String[] infoSplit = firstLineSplit[1].split("\\?");
+                            String[] typeSplit = infoSplit[1].split("&");
+                            String[] Input = new String[3];
+                            int count = 0;
+
+                            for (String string : typeSplit) {
+                                String[] input = string.split("=");
+                                Input[count] = input[1];
+                                count++;
+                            }
+
+                            //serverOutput = "HTTP/1.1 200 OK\r\n\r\n";
+
+                            if (Input[2] == null) {
+                                //serverOutput = serverOutput + TCPClient.askServer(Input[0], Integer.parseInt(Input[1]));
+                                serverOutput = TCPClient.askServer(Input[0], Integer.parseInt(Input[1]));
+                            }
+                            else {
+                                //serverOutput =
+                                //        serverOutput + TCPClient.askServer(Input[0], Integer.parseInt(Input[1]), Input[2]);
+                                serverOutput = TCPClient.askServer(Input[0], Integer.parseInt(Input[1]), Input[2]);
+                            }
+
+                            outToClient.writeBytes("HTTP/1.1 200 OK\r\n\r\n" + serverOutput);
+                            connectionSocket.close();
+                        }
+                        catch (UnknownHostException exception) {
+                            System.out.println(exception.fillInStackTrace());
+                            outToClient.writeBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+                            System.out.println("HTTP/1.1 404 Not Found\r\n\r\n");
+                            connectionSocket.close();
+                        }
+                        catch (NumberFormatException exception) {
+                            System.out.println(exception.fillInStackTrace());
+                            outToClient.writeBytes("HTTP/1.1 400 Bad Request\r\n\r\n");
+                            System.out.println("HTTP/1.1 400 Bad Request\r\n\r\n");
+                            connectionSocket.close();
+                        }
+                    }
+                    else {
+                        serverOutput = "HTTP/1.1 404 Not Found\r\n\r\n";
+                        outToClient.writeBytes(serverOutput);
+                        connectionSocket.close();
+                    }
+                }
+                catch (IOException exception) {
+                    exception.fillInStackTrace();
+                }
+            }).start();
+        }
+    }
+}
